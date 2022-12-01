4f2f586f070e913070297b4124f5cbfc3b3b5dd5
Zarbose <simonpieto.pieto@gmail.com>
2022-12-01 09:59:35 +0100

    add rainbow systems

---
+++ script/mvRainbowIntoWDSat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -u
+
+src_mydir=Documents/mq-comparison-suite/systems/.
+src=$HOME/$src_mydir/
+
+dest_mydir=Documents/WDSat/benchmarks/Rainbow
+dest=$HOME/$dest_mydir
+
+cp -r $src $dest
