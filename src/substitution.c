7c4378bcc1bbdbee8eabe6cc884d27ead657e320
Zarbose <simonpieto.pieto@gmail.com>
2022-12-20 16:43:43 +0100

    update substitution

--- src/substitution.c
+++ src/substitution.c
@@ -6,53 +6,62 @@
 #include "substitution.h"
 #include "dimacs.h"
 
+// Structure pour connaitre les équivalences
+uint_t substitution_equivalency[__SZ_VAR__][__SZ_SUB__];
+bool substitution_equivalent[__SZ_VAR__];
 
-uint_t substitution_equivalency[__ID_SIZE__][__SZ_SUB__];
-bool substitution_equivalent[__ID_SIZE__];
 
-static uint_t substitution_nb_of_vars;
+uint_t substitution_values[__SZ_VAR__][__MAX_ID__];
+bool substitution_index[__SZ_VAR__];
+
 static uint_t substitution_nb_of_equations;
 
-
+// substitution
 void substitution_fprint() {
 	uint_t v;
-	for(v = 1ULL; v <= substitution_nb_of_vars; ++v) {
+	for(v = 0LL; v <= substitution_nb_of_equations; ++v) {
         if (substitution_equivalent[v]){
-            printf(" %lls <=> ",v);
-            for (int_t j=0LL; j< __SZ_SUB__; j++) printf("%lld %lld\n",substitution_equivalency[v][0LL],substitution_equivalency[v][1LL]);
+            printf(" %lld <=> ",v);
+            for (int_t j=0LL; j< __SZ_SUB__; j++) printf("%lld ",substitution_equivalency[v][j]);
+            printf("\n");
         }
+        
 	}
 }
 
-// substitution
+inline void substitution_reset_boolean_vector(uint_t *v, uint_t sz) {
+	for(uint_t i = 0ULL; i < sz; ++i){
+		/// different writing for same aim
+		/// v[i] = 0ULL;
+		v[i] ^= v[i];
+    }
+}
+
 bool substitution_initiate_from_dimacs() {
-    const int_t _n_e = dimacs_nb_equations();
+    const int_t _n_e = dimacs_nb_equations(); // Ici _n_e = 900
 	substitution_nb_of_equations = _n_e;
 
-	// assert(substitution_nb_of_vars <= __MAX_ID__);
-	// assert(substitution_nb_of_equations <= __MAX_XEQ__);
-
     for(int_t i = 0LL; i <= _n_e; ++i) {
-        // Reset vector
+        substitution_reset_boolean_vector(substitution_equivalency[i],__SZ_SUB__);
 		substitution_equivalent[i] = false;
+        
+        substitution_reset_boolean_vector(substitution_values[i],__SZ_SUB__);
+		substitution_index[i] = false;
 	}
 
     for(int_t i = 0LL; i < _n_e; ++i) {
         int_t _s_e = dimacs_size_of_equation(i);
-        int_t * equation = dimacs_equation(i);
+        int_t * equation_buffer = dimacs_equation(i);
         if (_s_e == 3){
             substitution_equivalent[i]=true;
-            printf("1\n");
-            substitution_equivalency[i][0LL] = -equation[0LL]; // Ici erreur de segmentation
-            printf("2\n");
-            substitution_equivalency[i][1LL] = -equation[1LL];
-            printf("3\n");
+            substitution_equivalency[i][0LL] = -equation_buffer[0LL];
+            substitution_equivalency[i][1LL] = -equation_buffer[1LL];
         }
 	}
-    printf("FIN\n");
+    return (true);
 }
 
-// Ok
+/*
 void substitution_print_equivalency() {
 	int_t i;
     const int_t _n_e = dimacs_nb_equations();
@@ -68,4 +77,4 @@ void substitution_print_equivalency() {
         }
 		printf("\n");
 	}
-}
+}*/
--- src/substitution.h
+++ src/substitution.h
@@ -7,18 +7,17 @@
 
 #include "wdsat_utils.h"
 
-#define __SZ_SUB__ (__MAX_DEGREE__ - 1)
-
-extern uint_t substitution_equivalency[__ID_SIZE__][__SZ_SUB__];
-extern bool substitution_equivalent[__ID_SIZE__];
-
 // substitution
+#define __SZ_SUB__ (__MAX_DEGREE__ - 1)
+#define __SZ_VAR__ (900)
+
+extern uint_t substitution_equivalency[__SZ_VAR__][__SZ_SUB__];
+extern bool substitution_equivalent[__SZ_VAR__];
+
+extern uint_t substitution_values[__SZ_VAR__][__MAX_ID__];
+extern bool substitution_index[__SZ_VAR__];
+
 bool substitution_initiate_from_dimacs(void);
-void substitution_print_equivalency(void);
 void substitution_fprint(void);
-
-
-
-
-
+void substitution_reset_boolean_vector(uint_t *, uint_t sz);
 #endif
