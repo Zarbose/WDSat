0fb16d8226051e9ba77c0e69aec5ddfe0351a81e
Zarbose <simonpieto.pieto@gmail.com>
2023-05-11 14:04:47 +0200

    debut script stat

--- script/generate/calc_N_M.sh
+++ script/generate/calc_N_M.sh
@@ -2,7 +2,7 @@
 
 ### This script calculates N and M using o2 for rainbow systems
 
-[ $# -lt 1 ] && echo -e "\nUsage :./$(basename "$0") o2"
+[ $# -lt 1 ] && echo -e "Usage :\n      ./$(basename "$0") o2" && exit 1
 
 o2=$1
 
@@ -10,8 +10,3 @@ n=`expr $o2 - 2`
 m=`expr \( $o2 \* 2 \) - 1`
 
 echo "n = $n m = $m"
-
-# if ! [ -e "../../benchmarks/Rainbow/N_${n}_M_${m}" ];then
-#     echo "Création de : ../../benchmarks/Rainbow/N_${n}_M_${m}"
-#     mkdir "../../benchmarks/Rainbow/N_${n}_M_${m}"
-# fi
