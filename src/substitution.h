b627c1d95786ba44b1b81c7fa277a32b4d76ab6e
simon <simonpieto.pieto@gmail.com>
2023-08-21 10:07:09 +0200

    adding comments

--- src/substitution.h
+++ src/substitution.h
@@ -12,36 +12,40 @@
 #define __SZ_STACK__ 8*(__MAX_ANF_ID__*__MAX_ANF_ID__)
 
 // cnf module equivalence structures
-extern int_t substitution_equivalency_all[__ID_SIZE__][__SZ_SUB__];
-extern bool substitution_equivalent[__ID_SIZE__];
+// no_unary_var <=> unary_var
+extern int_t substitution_equivalency_all[__ID_SIZE__][__SZ_SUB__]; // Tableau qui contient des équivalences du type no_unary_var <=> unary_var
+extern bool substitution_equivalent[__ID_SIZE__]; // Tableau qui permet de savoir si une équivalence existe pour une variable donnée dans le tableau substitution_equivalency_all 
 
-extern int_t substitution_equivalency_unary[__MAX_ANF_ID__][__MAX_ID__];
-extern int_t substitution_equivalent_index_unary[__MAX_ANF_ID__];
+// unary_var <=> no_unary_var
+extern int_t substitution_equivalency_unary[__MAX_ANF_ID__][__MAX_ID__]; // Tableau qui contient des équivalences du type unary_var <=> no_unary_var
+extern int_t substitution_equivalent_index_unary[__MAX_ANF_ID__]; // Index qui permet de connaître la longueur d'une équivalence (ex : 26 <=> 3 1 4) dans le tableau substitution_equivalency_unary 
 
 // stack structures
-extern int_t substitution_up_stack[__ID_SIZE__];
-extern int_t substitution_up_top_stack;
-extern int_t *substitution_index_stack;
-extern int_t substitution_tag;
+extern int_t substitution_up_stack[__ID_SIZE__]; // Pile de propagation du module
+extern int_t substitution_up_top_stack; // Haut de la pile de propagation du module
+extern int_t *substitution_index_stack; // Index pour empêcher les doublons dans la pile de propagation du module
+extern int_t substitution_tag; // Tag utilisé dans l'index qui empêche les doublons
 
 // assignment structures
-extern boolean_t substitution_assignment_buffer[__SIGNED_ID_SIZE__];
-extern boolean_t *substitution_assignment;
+extern boolean_t substitution_assignment_buffer[__SIGNED_ID_SIZE__]; // Buffer du tableau d'assignement
+extern boolean_t *substitution_assignment; // Pointeur sur le buffert du tableau d'assignement
 
 // undo structures
+// undo assignement
 extern int_t substitution_history[__ID_SIZE__];
 extern int_t substitution_history_top;
 extern int_t substitution_step[__ID_SIZE__];
 extern int_t substitution_step_top;
 
-extern int_t *substitution_history_inte_stack;
-extern int_t substitution_history_inte_top;
+// undo substitution_values
+extern int_t *substitution_history_inte_stack; // Pile qui contient des couple de valeur (index, colonne concernée)
+extern int_t substitution_history_inte_top; // Haut de la pile substitution_history_inte_stack
 
-extern int_t substitution_history_main_stack[__ID_SIZE__];
+extern int_t substitution_history_main_stack[__ID_SIZE__]; // Pile qui permet de savoir jusqu'ou dépiler substitution_history_inte_stack
 extern int_t substitution_history_main_top;
 
-extern int_t *substitution_history_inte_index_stack;
-extern int_t substitution_history_tag;
+extern int_t *substitution_history_inte_index_stack; // Index pour empêcher les doublons dans substitution_history_inte_stack
+extern int_t substitution_history_tag; // Tag utilisé dans substitution_history_inte_index_stack pour empêcher les doublons
 
 
 #define _substitution_set(_v, _tv) \
